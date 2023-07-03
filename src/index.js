51e355af14fc2b3c3948f07ebd9ad7b3502ca14b
sangeetha <sangeethadhara1400@gmail.com>
2023-07-03 05:15:46 +0000

    technolgycards

---
+++ src/index.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import ReactDOM from 'react-dom'
+import App from './App'
+
+ReactDOM.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>,
+  document.getElementById('root'),
+)
+ 6 changes: 6 additions & 0 deletions6  ument.getElementById('root'),
+)
